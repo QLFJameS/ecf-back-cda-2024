098f249609fa04cb374bc2b0948c60615958e1fe
JameS <152168265+QLFJameS@users.noreply.github.com>
2024-08-01 14:25:05 +0200

    Update bdd McDo.sql

---
+++ bdd.sql
@@ -0,0 +1,258 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : lamp-mysql
+-- Généré le : jeu. 01 août 2024 à 12:23
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `MCDO`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Cart`
+--
+
+CREATE TABLE `Cart` (
+  `id` int NOT NULL,
+  `user_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Categorie`
+--
+
+CREATE TABLE `Categorie` (
+  `id` int NOT NULL,
+  `product_id` int DEFAULT NULL,
+  `name` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Commande`
+--
+
+CREATE TABLE `Commande` (
+  `id` int NOT NULL,
+  `cart_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `product_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Image`
+--
+
+CREATE TABLE `Image` (
+  `id` int NOT NULL,
+  `name` char(255) DEFAULT NULL,
+  `taille` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Langue`
+--
+
+CREATE TABLE `Langue` (
+  `id` int NOT NULL,
+  `name` varchar(8000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Product`
+--
+
+CREATE TABLE `Product` (
+  `id` int NOT NULL,
+  `image_id` int DEFAULT NULL,
+  `categorie_id` int DEFAULT NULL,
+  `name` char(255) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `description` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Role`
+--
+
+CREATE TABLE `Role` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utilisateur`
+--
+
+CREATE TABLE `Utilisateur` (
+  `id` int NOT NULL,
+  `langue_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  `cart_id` int DEFAULT NULL,
+  `order_id` int DEFAULT NULL,
+  `name` char(255) DEFAULT NULL,
+  `email` char(255) DEFAULT NULL,
+  `passeword` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Cart`
+--
+ALTER TABLE `Cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_CartUser` (`user_id`);
+
+--
+-- Index pour la table `Categorie`
+--
+ALTER TABLE `Categorie`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_CategorieProduct` (`product_id`);
+
+--
+-- Index pour la table `Commande`
+--
+ALTER TABLE `Commande`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_UserOrder` (`user_id`),
+  ADD KEY `FK_ProductOrder` (`product_id`);
+
+--
+-- Index pour la table `Image`
+--
+ALTER TABLE `Image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Langue`
+--
+ALTER TABLE `Langue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_ProductImage` (`image_id`),
+  ADD KEY `FK_ProductCategorie` (`categorie_id`);
+
+--
+-- Index pour la table `Role`
+--
+ALTER TABLE `Role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_UserRole` (`role_id`),
+  ADD KEY `FK_UserLangue` (`langue_id`),
+  ADD KEY `FK_UserOrder` (`order_id`),
+  ADD KEY `FK_UserCart` (`cart_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Cart`
+--
+ALTER TABLE `Cart`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Categorie`
+--
+ALTER TABLE `Categorie`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Product`
+--
+ALTER TABLE `Product`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `Cart`
+--
+ALTER TABLE `Cart`
+  ADD CONSTRAINT `FK_CartUser` FOREIGN KEY (`user_id`) REFERENCES `Utilisateur` (`id`);
+
+--
+-- Contraintes pour la table `Categorie`
+--
+ALTER TABLE `Categorie`
+  ADD CONSTRAINT `FK_CategorieProduct` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+
+--
+-- Contraintes pour la table `Commande`
+--
+ALTER TABLE `Commande`
+  ADD CONSTRAINT `FK_CommandeUtilisateur` FOREIGN KEY (`user_id`) REFERENCES `Utilisateur` (`id`),
+  ADD CONSTRAINT `FK_ProductOrder` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+
+--
+-- Contraintes pour la table `Product`
+--
+ALTER TABLE `Product`
+  ADD CONSTRAINT `FK_ProductCategorie` FOREIGN KEY (`categorie_id`) REFERENCES `Categorie` (`id`),
+  ADD CONSTRAINT `FK_ProductImage` FOREIGN KEY (`image_id`) REFERENCES `Image` (`id`);
+
+--
+-- Contraintes pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  ADD CONSTRAINT `FK_UserCart` FOREIGN KEY (`cart_id`) REFERENCES `Cart` (`id`),
+  ADD CONSTRAINT `FK_UserLangue` FOREIGN KEY (`langue_id`) REFERENCES `Langue` (`id`),
+  ADD CONSTRAINT `FK_UserOrder` FOREIGN KEY (`order_id`) REFERENCES `Commande` (`id`),
+  ADD CONSTRAINT `FK_UserRole` FOREIGN KEY (`role_id`) REFERENCES `Utilisateur` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
